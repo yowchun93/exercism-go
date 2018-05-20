273f1a5ec25fea4385f8adbedb4379ab32035b1d
yowchun <yowchun93@gmail.com>
2018-05-20 13:34:13 +0800

    incomplete solution

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -6,7 +6,9 @@
 package bob
 
 import (
+	"unicode"
 	"strings"
+
 )
 
 // Hey should have a comment documenting it.
@@ -16,11 +18,11 @@ func Hey(remark string) string {
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
 	reply := ""
-	if (strings.Title(remark) == remark) && (strings.Contains(remark,"?")) {
+	if isQuestion(remark) && allUpperCase(remark) {
 		reply = "Calm down, I know what I'm doing!"
-	} else if strings.Contains(remark, "?") {
+	} else if isQuestion(remark) {
 		reply = "Sure."
-	} else if strings.Title(remark) == remark {
+	} else if any(remark, unicode.IsUpper) && !any(remark, unicode.IsLower) {
 		reply = "Whoa, chill out!"
 	} else {
 		reply = "Whatever."
@@ -33,3 +35,48 @@ func Hey(remark string) string {
 
 //"1, 2, 3"
 //"Whoa, chill out"
+func any(items string, test func(rune) bool) bool {
+	for _, item := range items {
+	  if test(item) {
+		return true
+	  }
+	}
+	return false
+}
+
+func allUpperCase(sentence string) bool {
+	// return if the string contains number
+	if isInt(sentence){
+		return false
+	} else if strings.ToUpper(sentence) == sentence {
+		return true
+	}
+	return false
+}	
+
+func isInt(s string) bool {
+    for _, c := range s {
+        if unicode.IsDigit(c) {
+            return true
+        }
+    }
+    return false
+}
+
+func isNonLetter(s string) bool {
+	for _, c := range s {
+		if !unicode.IsLetter(c) && !unicode.IsDigit(c) {
+            return true
+        }
+    }
+    return false
+}
+
+// isQuestion 
+func isQuestion(sentence string) bool {
+	if sentence[len(sentence)-1] == '?' {
+		return true
+	} else {
+		return false
+	}
+}
